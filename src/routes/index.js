e1be4ddcbc149b17083ec109d8bb4315eba7f61b
nghuyhoang2509 <nghuyhoang2509@gmail.com>
2022-07-14 18:27:47 +0700

    css and router

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
